6ccd613353058a617ba7800afa6b0eac5d7ba938
Dan Irani <dan.irani@fastdelta.com>
2021-01-18 16:23:36 +0000

    Updated API to return task array.

--- Handler.cs
+++ Handler.cs
@@ -1,25 +1,38 @@
+using System.Collections;
 using Amazon.Lambda.Core;
+using System.Collections.Generic;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
+       public ArrayList Hello(Request request)
        {
-           return new Response("Let's Recycle API Called Successfully", request);
+           ArrayList tasks = new ArrayList();
+           Task t1 = new Task("XA-1001", "Visit Manchester", false);
+           Task t2 = new Task("XA-1002", "Visit London", false);
+           Task t3 = new Task("XA-1003", "Visit Liverpool", false);
+
+           tasks.Add(t1);
+           tasks.Add(t2);
+           tasks.Add(t3);
+           return tasks;
        }
     }
 
-    public class Response
+    public class Task
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
-
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
-      }
+        public string TaskId { get; }
+        public string Description { get; }
+        public bool Completion { get; }
+        
+        public Task(string _taskId, string _description, bool _completion)
+        {
+            TaskId = _taskId;
+            Description = _description;
+            Completion = _completion;
+        }
     }
 
     public class Request
