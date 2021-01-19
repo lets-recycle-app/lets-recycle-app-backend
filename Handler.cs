342d43087d0412c3d5bc53d2024bf232791bf86c
Dan Irani <dan.irani@fastdelta.com>
2021-01-19 08:12:58 +0000

    Added path parameter check to switch Http Get results depending on user.

--- Handler.cs
+++ Handler.cs
@@ -1,22 +1,37 @@
 using System.Collections;
 using Amazon.Lambda.Core;
 using System.Collections.Generic;
+using Amazon.Lambda.APIGatewayEvents;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public ArrayList Tasks(Request request)
+        
+       public ArrayList GetTasks(APIGatewayProxyRequest request)
        {
+           string userId = request.PathParameters["userId"];
            ArrayList tasks = new ArrayList();
-           Task t1 = new Task("XA-1001", "Visit Manchester", false);
-           Task t2 = new Task("XA-1002", "Visit London", false);
-           Task t3 = new Task("XA-1003", "Visit XLiverpool", false);
 
-           tasks.Add(t1);
-           tasks.Add(t2);
-           tasks.Add(t3);
+           if (userId == "dan")
+           {
+               Task t1 = new Task("GR-1001", "Visit Greece", false);
+               tasks.Add(t1);
+           }
+           else
+           {
+               Task t1 = new Task("UK-1001", "Visit Manchester", false);
+               Task t2 = new Task("UK-1002", "Visit London", false);
+               Task t3 = new Task("UK-1003", "Visit XLiverpool", false);
+               
+               tasks.Add(t1);
+               tasks.Add(t2);
+               tasks.Add(t3);
+           }
+           
+           //LambdaLogger.Log("Getting Tasks For " +userId);
+           
            return tasks;
        }
     }
