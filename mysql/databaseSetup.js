3f5448fd87edc6f0f3df5ff96481b7c7ce267de9
Dan Irani <dan.irani@fastdelta.com>
2021-01-20 10:27:42 +0000

    Added table routes, drivers and addresses;

--- mysql/databaseSetup.js
+++ mysql/databaseSetup.js
@@ -1,5 +1,5 @@
 import { dbControl } from './dbControl.js';
-import { createUsers, createCollRequest, createDepots } from './databaseSqls.js';
+import { createUsers, createCollRequest, createDepots, createAll } from './databaseSqls.js';
 
 const db = dbControl();
 db.setRegion('eu-west-2');
@@ -8,3 +8,4 @@ db.setInstance('prod-mysql');
 db.runSql(createUsers);
 db.runSql(createCollRequest);
 db.runSql(createDepots);
+db.runSql(createAll);
--- mysql/databaseSqls.js
+++ mysql/databaseSqls.js
@@ -69,15 +69,15 @@ drop table if exists depots;
 
 create table depots (
   id int not null auto_increment,
-  name varchar(20) not null,
-  postCode varchar(30) not null,
-  fleetSize varchar(30) null,
+  depotName varchar(50) not null,
+  postCode varchar(10) not null,
+  fleetSize int not null,
   primary key (id)
 );
 
 alter table depots auto_increment=1;
 
-insert into depots (name, postCode, fleetSize) 
+insert into depots (depotName, postCode, fleetSize) 
 values
 
 ('Horwich', 'HO1 8XJ', 26),
@@ -100,5 +100,46 @@ values
 ('Reading', 'RG1 3YL', 13),
 ('Blackburn', 'BB2 1NA', 19),
 ('York', 'YO10 3FQ', 9);
+`;
 
+export const createAll = `
+
+drop table if exists routes;
+
+create table routes (
+    depotId int not null,                   /* depot Id from table depots */
+    driverId int not null,                  /* driver Id from table drivers */
+    routeDate date not null,                /* route date */
+    addressId int not null,                 /* address Id from table addresses */
+    routeSeqNo int not null,                /* delivery or recycle seq no. in route 1,2,3,... */
+    routeAction varchar(10) not null,       /* deliver or recycle */
+    itemType varchar(30) not null,          /* fridge, freezer etc. */
+    status varchar(10) not null,            /* pending, completed, failed */
+    primary key (depotId, driverId, routeDate)
+);
+
+drop table if exists drivers;
+
+create table drivers (
+    driverId int not null auto_increment,   /* driverId auto created */
+    driverName varchar(50) not null,        /* free format driver name */
+    truckSize int not null,                 /* cubic capacity of truck */
+    primary key (driverId)
+);
+    
+alter table drivers auto_increment=1;
+
+drop table if exists addresses;
+
+create table addresses (
+    addressId int not null auto_increment,  /* driverId auto created */
+    postcode varchar(10) not null,          /* a valid postcode */
+    customerName varchar(50) not null,      /* free format customer name */
+    customerEmail varchar(100) not null,    /* customer email address */
+    locationType varchar(10) not null,      /* "private" or "public" address type */  
+    fullAddress varchar(200) not null,      /* free format address details */
+    primary key (addressId)
+);
+    
+alter table addresses auto_increment=1;
 `;
