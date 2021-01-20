824198e2dbdcd26edc04aa3af60b7bb7af3a4826
Dorota <d.a.lewinska@gmail.com>
2021-01-20 15:42:06 +0000

    Zip name amended for bash (from hello.zip to data-api.zip)

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ fi
 
 dotnet restore
 dotnet tool install -g Amazon.Lambda.Tools --framework netcoreapp3.1
-dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/Release/netcoreapp3.1/hello.zip
+dotnet lambda package --configuration Release --framework netcoreapp3.1 --output-package bin/Release/netcoreapp3.1/data-api.zip
 read a
