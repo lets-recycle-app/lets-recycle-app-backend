d2d7374f4ad45e95290ddafcf1a49a9933877925
Dan Irani <dan.irani@fastdelta.com>
2021-01-31 14:48:13 +0000

    Consolidated all http methods into one handler.

--- app/LambdaApi/LambdaApi.cs
+++ app/LambdaApi/LambdaApi.cs
@@ -1,4 +1,3 @@
-using System;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
 using Amazon.Lambda.Serialization.SystemTextJson;
@@ -10,10 +9,8 @@ namespace LambdaApi
 {
     public class LambdaApi
     {
-        public APIGatewayProxyResponse ApiGet(APIGatewayProxyRequest request)
+        public APIGatewayProxyResponse Api(APIGatewayProxyRequest request)
         {
-            string apiRoute = request.PathParameters["api"];
-
             RouteFarm routeFarm = new RouteFarm(request.HttpMethod, FullEndpoint(request));
 
             return new APIGatewayProxyResponse
@@ -24,50 +21,35 @@ namespace LambdaApi
             };
         }
 
-        private string FullEndpoint(APIGatewayProxyRequest request)
+        private static string FullEndpoint(APIGatewayProxyRequest request)
         {
+            // lambda proxy handler separates query string from 
+            // Rest endpoint. Recreate the the full path to allow
+            // this lambda to interface in the same way as the local
+            // test environment.
+            
             string fullEndpoint = request.Path.Replace("%20", "");
-
+            
+            if (request.QueryStringParameters == null) return fullEndpoint;
+            
             int count = 0;
-
-            if (request.QueryStringParameters != null)
+            
+            foreach (var (key, value) in request.QueryStringParameters)
             {
-                foreach (var (key, value) in request.QueryStringParameters)
+                if (count > 0)
                 {
-                    if (count > 0)
-                    {
-                        fullEndpoint += "&";
-                    }
-                    else
-                    {
-                        fullEndpoint += "?";
-                    }
-
-                    fullEndpoint += $"{key}={value}";
-                    count += 1;
+                    fullEndpoint += "&";
                 }
+                else
+                {
+                    fullEndpoint += "?";
+                }
+
+                fullEndpoint += $"{key}={value}";
+                count += 1;
             }
 
             return fullEndpoint;
-        }
-
-        public APIGatewayProxyResponse ApiPost(APIGatewayProxyRequest request)
-        {
-            string apiRoute = request.PathParameters["api"];
-
-            Console.WriteLine("-------------- P O S T ------------------");
-            Console.WriteLine($"Api Post Route [{apiRoute}]");
-            Console.WriteLine($"Body {request.Body}");
-
-
-            RouteFarm routeFarm = new RouteFarm("GET", apiRoute);
-
-            return new APIGatewayProxyResponse
-            {
-                Body = routeFarm.ResponseJson.Body,
-                Headers = routeFarm.ResponseJson.Headers,
-                StatusCode = routeFarm.ResponseJson.StatusCode
-            };
         }
     }
 }
